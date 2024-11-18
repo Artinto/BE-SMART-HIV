c2d63682401a47c8ee5dae69cf8f7c8dcdacf065
Artinto <42999398+Artinto@users.noreply.github.com>
2024-11-18 13:27:34 +0900

    Update model.py

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from torchvision.models import resnet18
 
 
-class CE(nn.Module):
+class BCE(nn.Module):
     def __init__(self):
         super(CE, self).__init__()
         self.resnet = resnet18(pretrained='ResNet18_Weights.IMAGENET1K_V1')
@@ -13,7 +13,7 @@ class CE(nn.Module):
         self.fc1_drop = nn.Dropout()
         self.fc2 = nn.Linear(400, 84)
         self.fc2_drop = nn.Dropout()
-        self.fc3 = nn.Linear(84, 4)
+        self.fc3 = nn.Linear(84, 1)
 
     def forward(self, x):
         x = self.resnet(x)
@@ -21,7 +21,6 @@ class CE(nn.Module):
         x = self.fc2_drop(F.relu(self.fc2(x)))
         x = self.fc3(x)
         return x
-
 
     def init_weights(model):  # weight initialization
         w = torch.empty(3, 5)
